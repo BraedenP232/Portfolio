eecce1096a8eaf7d8daa21d12f34b9f6b0041c7c
Braeden Pelletier <braedenp232@gmail.com>
2022-03-24 00:17:00 -0600

    added dynamic dark light toggle and accessability

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
-/* Add your own script here */
+// get the element
+const element = document.getElementById('info_click')
+// set isdark to true or false once
+var isdark = (window?.matchMedia('(prefers-color-scheme: dark)').matches)
+// always checking if the element is clicked
+element.addEventListener("click", () => {
+    function toggleDarkMode() {
+          if (isdark) {
+            // reset style
+            document.documentElement.style.display = 'none';
+            // if dark mode is on, set to light mode css
+            document.head.insertAdjacentHTML(
+            'beforeend',
+            '<link rel="stylesheet" href="css/light.css" onload="document.documentElement.style.display = \'\'">',
+            // toggle variable because user's device will still be in dark mode
+            isdark = false
+          );
+          } else {
+            // reset style
+            document.documentElement.style.display = 'none';
+            // if light mode is on, set to dark mode css
+            document.head.insertAdjacentHTML(
+            'beforeend',
+            '<link rel="stylesheet" href="css/dark.css" onload="document.documentElement.style.display = \'\'">',
+            // toggle variable because user's device will still be in light mode
+            isdark = true
+          );
+          }
+        
+      }
+    toggleDarkMode()
+});
+
+
+// Function to set background to constant color due to distraction
+// Color still varies by dark/light mode
+const btn = document.getElementById('distraction')
+btn.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    document.body.style.background = 'var(--background-color)';
+  
+    // 👇️ optionally change text color
+    // document.body.style.color = 'white';
+    btn.style.display = 'none';
+  });
